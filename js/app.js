5614376cbd014736bf879e7b3a47a218cdf3951c
Ashwini Rao <ashkrao@gmail.com>
2017-06-06 23:28:01 -0700

    Added questions 2 & 3

--- js/app.js
+++ js/app.js
@@ -1,20 +1,35 @@
 'use strict';
 var userScore = 0;
 
-console.log('Ready to have some fun!');
-
 alert('Welcome to my guessing game!');
 
 var user = prompt('What is your name?');
 
 alert('Hi ' + user + '! Nice to meet you!');
 
-var answer1 = prompt('Does Ashwini like pineapple?', 'type yes or no').toLowerCase();
-console.log(answer1);
+var answer = prompt('Does Ashwini like pineapple?', 'type yes or no').toLowerCase();
+console.log(answer);
 
-if (answer1 === 'yes' || answer1 === 'y') {
+if (answer === 'yes' || answer === 'y') {
   userScore++;
   alert('You got that right!')
 } else {
   alert('Wrong! go to the next question');
+}
+
+answer = prompt('Is Ashwini\'s favorite color purple?', 'type yes or no').toLowerCase();
+console.log(answer);
+if (answer === 'no' || answer === 'n') {
+  userScore++;
+  alert('You got that right!')
+} else {
+  alert('Wrong!');
+}
+answer = prompt('Does Ashwini like chocolate?', 'type yes or no').toLowerCase();
+console.log(answer);
+if (answer === 'yes' || answer === 'y') {
+  userScore++;
+  alert('You got that right!')
+} else {
+  alert('Wrong!');
 }
