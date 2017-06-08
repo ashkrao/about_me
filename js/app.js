0fab8084a3c6709f816fa058e0618e4720f84883
Ashwini Rao <ashkrao@gmail.com>
2017-06-08 00:58:55 -0700

    Added 6th question, updated total score message

--- js/app.js
+++ js/app.js
@@ -7,12 +7,12 @@ var user = prompt('What is your name?');
 
 alert('Hi ' + user + '! Nice to meet you!');
 
-var q1 = 'Does Ashwini like pineapple?';
-var q2 = 'Is Ashwini\'s favorite color purple?';
+var q1 = 'Is Ashwini a good developer?';
+var q2 = 'Can Ashwini write clean code?';
 var q3 = 'Does Ashwini like sports?';
 var q4 = 'Does Ashwini like chocolate?';
 var q5 = 'Is Ashwini going to Ace code201? (You better say yes)';
-
+var q6 = 'What is my favorite number?';
 
 var answer = prompt(q1, 'type yes or no').toLowerCase();
 console.log('Prompted: ' + q1);
@@ -20,22 +20,23 @@ console.log('Got this answer: ' + answer);
 
 if (answer === 'yes' || answer === 'y') {
   userScore++;
-  alert('You got that right!');
+  alert('You betcha!');
   console.log('Correct answer!');
 } else {
   alert('Wrong! go to the next question');
+  console.log('Wrong answer!');
 }
 
 answer = prompt(q2, 'type yes or no').toLowerCase();
 console.log('Prompted: ' + q2);
 console.log('Got this answer: ' + answer);
 
-if (answer === 'no' || answer === 'n') {
+if (answer === 'yes' || answer === 'y') {
   userScore++;
-  alert('You got that right!');
+  alert('You betcha!');
   console.log('Correct answer!');
 } else {
-  alert('Wrong!');
+  alert('Wrong! You need to see her code to believe it!');
   console.log('Wrong answer!');
 }
 
@@ -77,4 +78,23 @@ if (answer === 'yes' || answer === 'y') {
   alert('You are doomed!');
   console.log('Wrong answer!');
 }
-alert('Your total score is : ' + userScore);
+
+var guesses = 4;
+var favNumber = 81;
+while(guesses > 0) {
+  answer = prompt(q6 + ' You have ' + guesses + ' guesses.', 'enter a number');
+  if(answer == favNumber) {
+    alert('Bingo!');
+    userScore++;
+    break;
+  } else {
+    guesses--;
+    if(answer > favNumber) {
+      alert('That\'s too high!');
+    } else {
+      alert('That\'s too low!');
+    }
+  }
+}
+
+alert('Your total score is : ' + userScore + ' out of 6, ' + user + '!');
